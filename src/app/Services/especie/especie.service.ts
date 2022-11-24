ff69cb0d5c307a6bc4b4b2d4c66ca73443f4765a
klxiomy <kristinxiomararivera20@gmail.com>
2022-11-24 17:47:49 -0500

    Servicio de eliminar,actualizar y listar especies

--- src/app/Services/especie/especie.service.ts
+++ src/app/Services/especie/especie.service.ts
@@ -8,12 +8,29 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class EspecieService {
-   Api = pathUrlService
+  
   constructor(private httpClient:HttpClient) { }
 
 
   listarEspecies():Observable<responseApi>{
     let url = 'allespecies'
     return this.httpClient.get<responseApi>(pathUrlService + url)
+  }
+
+  deleteEspecie(id_especie:number):Observable<responseApi>{
+    let url = 'deleteespecie'
+    return this.httpClient.delete<responseApi>(pathUrlService + url + '/' + id_especie)
+  }
+  actualizarEspecie(id_especie:number, especie: any):Observable<responseApi>{
+    let url = 'updateespecie'
+    return this.httpClient.put<responseApi>(pathUrlService + url + '/' + id_especie, especie)
+  }
+  getEspecieById(id_especie:string):Observable<responseApi>{
+    let url = 'oneespecie'
+    return this.httpClient.get<responseApi>(pathUrlService + url + '/' + id_especie)
+  }
+  updateEspecie(id_especie:number, especie:any):Observable<responseApi>{
+    let url = 'updateespecie'
+    return this.httpClient.put<responseApi>(pathUrlService + url + '/' + id_especie, especie)
   }
 }
