7aced6733116243ab5db8ae0e8a1ebc3a240e5df
yicxelaconejo <yichconejo@misena.edu.co>
2022-11-24 18:11:43 -0500

    Changes

--- src/app/views/nutrientes/registro/registro.component.ts
+++ src/app/views/nutrientes/registro/registro.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { NutrienteService } from '../../../Services/nutrientes/nutriente.service';
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.component.html',
-  styleUrls: ['./registro.component.css']
+  styleUrls: ['./registro.component.css'],
 })
 export class RegistroComponent implements OnInit {
+  postFormulario = new FormGroup({
+    nombre_tipo_nutriente: new FormControl('', Validators.required),
+  });
+  constructor(private apiservice: NutrienteService, private router:Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
+  postForm(form: any) {
+    if (this.postFormulario.valid) {
+      this.apiservice.PostNutrientes(form).subscribe((data) => {
+        console.log(data);
+        this.router.navigate(['nutrientes'])
+      });
+    } else {
+      console.log("Campos Obligatorios");
 
-  ngOnInit(): void {
+    }
+
+    console.log(form);
   }
-
 }
