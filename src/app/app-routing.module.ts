ef64fbeef4805aaf728919e07e92e6b018be18f6
Nonimus <jpceron0@misena.edu.co>
2022-11-23 23:38:41 -0500

    add guard and services folder also canActicate in routes para habilitar el guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './views/login/login.component';
 import { DashboardComponent } from './component/dashboard/dashboard.component';
+import { VigilantGuard } from './Guards/vigilant.guard';
 
 const routes: Routes = [
   { path: "", redirectTo: 'login', pathMatch: 'full' },
@@ -12,6 +13,7 @@ const routes: Routes = [
   {
     path: 'dashboard',
     component: DashboardComponent,
+    canActivate : [VigilantGuard ] ,
     children: [
       {
         // path: 'login', childrens
