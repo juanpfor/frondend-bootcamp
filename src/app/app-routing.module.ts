7105b50fb2bd74d28de88b655606105c21b9141d
Nonimus <jpceron0@misena.edu.co>
2022-11-23 23:15:50 -0500

    fist commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './views/login/login.component';
+import { DashboardComponent } from './component/dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: 'login', pathMatch: 'full' },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    children: [
+      {
+        // path: 'login', childrens
+        // component: LoginComponent,
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { DashboardComponent } from './component/dashboard/dashboard.component'
+import { HeaderComponent } from './component/header/header.component'
+import { FooterComponent } from './component/footer/footer.component'
+import { ViewsComponent } from './views/views.component'
+import { LoginComponent } from './views/login/login.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    HeaderComponent,
+    FooterComponent,
+    ViewsComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
