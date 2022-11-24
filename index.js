c95c818428c2779cc6e43eb47d3b9230d2f08d66
Nonimus <jpceron0@misena.edu.co>
2022-11-23 23:24:05 -0500

    add changes

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+
+
+
+
+const PORT = 8000
+
+app.listen(err => {
+  if (err) {
+    console.log(`eror de seculize `);
+  }  else{
+    console.log(`server in runiig in port ${PORT}`);
+
+  }
+})
