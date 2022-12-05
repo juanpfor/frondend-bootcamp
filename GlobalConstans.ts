80250a1113c06123730ebb1f4b263ce065cd775b
Nonimus <jpceron0@misena.edu.co>
2022-12-05 07:39:45 -0600

    changes in constastas

--- GlobalConstans.ts
+++ GlobalConstans.ts
@@ -1,5 +1,5 @@
-export const pathUrlService = "http://localhost:8057/api/"
+export const pathUrlService = "http://152.200.173.74:8057/api"
 
-// http://192.168.192.57:8057/api/  //config globla serve
-
+// http://192.168.192.57:8057/api/  //config globla serve private
+//http://152.200.173.74:8057/api //config url public server
 // http://localhost:8057/api/ //config local
